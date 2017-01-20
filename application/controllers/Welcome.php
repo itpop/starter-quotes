22a509600886928d1342cae2085c4fe600fe5586
Robert Arendac <robertarendac@gmail.com>
2017-01-20 15:55:27 -0800

    Revert "Added a random quote display"
    This reverts commit b573cba5dccd2596fec27223c36d919080b2a3b7.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -29,22 +29,5 @@ class Welcome extends Application
 
 		$this->render();
 	}
-	
-	public function random()
-	{
-		$this->data['pagebody'] = 'homepage';
-		
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-		$authors = array ();
-		
-		//Pick one person at random
-		$randomPerson = $source[array_rand($source)];
-		$author[] = array ('who' => $randomPerson['who'], 'mug' => $randomPerson['mug'], 'href' => $randomPerson['where'], 'what' => $randomPerson['what']);
-		
-		$this->data['authors'] = $author;
-
-		$this->render();
-	}
 
 }
--- changelog.md
+++ changelog.md
@@ -1,5 +1,3 @@
 Created a team for lab2: Fred & Rob
 
 Added a new picture and quote for Homer Simpson
-
-Added the random quote feature
